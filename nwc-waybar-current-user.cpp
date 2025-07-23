c82bcd8cf9a3c6b708f0ca036a4f03b035819903
Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
2025-07-23 05:05:08 +0200

    Add `--ignore-second` option to omit seconds in uptime output

--- nwc-waybar-current-user.cpp
+++ nwc-waybar-current-user.cpp
@@ -1,19 +1,19 @@
 #include <chrono>
-#include <iostream>
 #include <unistd.h>
 #include <pwd.h>
 #include <string>
 #include <print>
 #include <systemd/sd-login.h>
+#include <boost/json.hpp>
 
 #include "nwc/arguments.hpp"
 #include "nwc/utility.hpp"
-#include <boost/json.hpp>
 
 namespace po = boost::program_options;
 
 static bool dont_add_user_icon = false;
 static bool append_uptime_to_text = false;
+static bool ignore_seconds = false;
 static std::string icon_str;
 
 void loop();
@@ -26,12 +26,17 @@ int main(int argc, char **argv) {
              "don't add icon to response")
             ("icon-to-add", po::value(&icon_str)->default_value("\uf007"), "which icon to add")
             ("append-uptime-to-text", po::bool_switch(&append_uptime_to_text)->default_value(false),
-             "append uptime to text instead of using tooltip");
+             "append uptime to text instead of using tooltip")
+            ("ignore-second", po::bool_switch(&ignore_seconds)->default_value(false), "dont output seconds");
 
     args.parse(argc, argv);
     if (args.help()) {
         args.show_help();
         return 1;
+    }
+
+    if (args.sleep_for() >= 60000) {
+        ignore_seconds = true;
     }
 
     do {
@@ -77,22 +82,44 @@ std::string convert_duration_tostring(std::chrono::duration<T, U> duration) {
     using namespace std::chrono;
 
     if (duration >= days{1}) {
+        if (ignore_seconds) {
+            return std::format("{}d {:02d}h {:02d}m",
+                               duration_cast<days>(duration).count(),
+                               duration_cast<hours>(duration).count() % 24,
+                               duration_cast<minutes>(duration).count() % 60
+            );
+        }
+
         return std::format("{}d {:02d}h {:02d}m {:02d}s", duration_cast<days>(duration).count(),
                            duration_cast<hours>(duration).count() % 24,
                            duration_cast<minutes>(duration).count() % 60,
                            duration_cast<seconds>(duration).count() % 60);
     }
     if (duration >= hours{1}) {
-        return std::format("{:02d}h {:02d}m {:02d}s", duration_cast<hours>(duration).count() % 24,
+        if (ignore_seconds) {
+            return std::format("{}h {:02d}m",
+                               duration_cast<hours>(duration).count(),
+                               duration_cast<minutes>(duration).count() % 24
+            );
+        }
+
+        return std::format("{}h {:02d}m {:02d}s", duration_cast<hours>(duration).count() % 24,
                            duration_cast<minutes>(duration).count() % 60,
                            duration_cast<seconds>(duration).count() % 60
         );
     }
     if (duration >= minutes{1}) {
-        return std::format("{:02d}m {:02d}s", duration_cast<minutes>(duration).count() % 60,
+        if (ignore_seconds) {
+            return std::format("{}m",
+                               duration_cast<minutes>(duration).count() % 24
+            );
+        }
+
+        return std::format("{}m {:02d}s", duration_cast<minutes>(duration).count() % 60,
                            duration_cast<seconds>(duration).count() % 60
         );
     }
+
     return std::format("{:02d}s", duration_cast<seconds>(duration).count() % 60);
 }
 
